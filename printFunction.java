3ce84e8b5038fe571c65446d54ebc84116b2e7e5
AyushiR0y <132144705+AyushiR0y@users.noreply.github.com>
2023-08-06 21:42:54 +0530

    Create printFunction.java

---
+++ printFunction.java
@@ -0,0 +1,30 @@
+// This is the class named School
+class School { 
+    // This is a method in class School
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+} 
+// This is the class named Student
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT"); 
+    } 
+}
+
+public class Question21{ 
+    public static void main(String args[]){
+               // Create an object of the Student class
+        Student student = new Student();
+        
+        // Call the print() method of the Student class
+        student.print();
+        
+        // Create an object of the School class
+        School school = new School();
+        
+        // Call the print() method of the School class
+        school.print();
+
+
